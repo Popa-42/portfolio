dd1995ea735057d7e181882a5459e2d0e2120e10
Popa <115492561+Popa-42@users.noreply.github.com>
2025-06-05 08:48:25 +0200

    Add button, header, tooltip components and update footer layout

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,30 +2,30 @@ import Link from "next/link";
 
 export function SiteFooter() {
   return (
-    <footer className="h-fit min-h-15 w-full border-t py-6 md:py-0">
+    <footer className="bg-background h-fit min-h-15 w-full border-t py-6 md:py-0">
       <div className="flex h-full min-h-15 items-center justify-center">
-        <div className="container">
+        <div className="container px-4">
           <div
             className={`
-              text-muted-foreground grid items-center text-sm leading-loose text-balance gap-2
+              text-muted-foreground grid items-center gap-2 text-sm leading-loose text-balance
               md:grid-cols-3 md:text-left
             `}
           >
-            <span className="text-center md:text-left">
+            <span className="text-center md:text-start">
               © {new Date().getFullYear()} Popa-42. All rights reserved.
             </span>
             <span className="text-center">
               Made with 💜 by <Link href="/">Popa</Link>.
             </span>
-            <span className="text-center md:text-right">
+            <span className="space-x-2 text-center md:text-end">
               <Link href="https://github.com/Popa-42" target="_blank" rel="noopener noreferrer">
                 GitHub
               </Link>
-              {" | "}
+              <span>|</span>
               <Link href="https://example.com" target="_blank" rel="noopener noreferrer">
                 Useless link
               </Link>
-              {" | "}
+              <span>|</span>
               <Link href="https://example.org" target="_blank" rel="noopener noreferrer">
                 Another one
               </Link>

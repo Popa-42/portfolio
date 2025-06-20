57b2baab557f95db517287409ad02e503484d5da
Popa <115492561+Popa-42@users.noreply.github.com>
2025-06-20 20:26:35 +0200

    Add dark mode toggle to header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Moon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useDarkMode } from "@/hooks/dark-mode";
 
 export default function Header() {
   const router = useRouter();
+  const [darkMode, toggleDarkMode] = useDarkMode();
 
   return (
     <header className="fixed top-0 left-0 z-10 flex w-full items-center justify-center py-4">
@@ -26,89 +29,94 @@ export default function Header() {
           </Link>
         </div>
         <div className="h-0 w-full border-b border-dashed border-foreground/25" />
-        <nav className="flex rounded-md backdrop-blur-sm">
-          <Button
-            size="icon"
-            variant="outline"
-            className={`
-              size-10 backdrop-blur-none
-              not-first:rounded-s-none not-first:border-s-0
-              not-last:rounded-e-none not-last:border-e-0
-            `}
-            onClick={() => router.push("https://s.p0pa.de/gh")}
-          >
-            <Image
-              draggable={false}
-              className="dark:hidden"
-              src="/assets/images/github.svg"
-              alt="GitHub logo"
-              width={17}
-              height={17}
-            />
-            <Image
-              draggable={false}
-              className="hidden dark:inline-block"
-              src="/assets/images/github_white.svg"
-              alt="GitHub logo"
-              width={17}
-              height={17}
-            />
+        <div className="flex gap-4">
+          <nav className="flex rounded-md backdrop-blur-sm">
+            <Button
+              size="icon"
+              variant="outline"
+              className={`
+                size-10 backdrop-blur-none
+                not-first:rounded-s-none not-first:border-s-0
+                not-last:rounded-e-none not-last:border-e-0
+              `}
+              onClick={() => router.push("https://s.p0pa.de/gh")}
+            >
+              <Image
+                draggable={false}
+                className="dark:hidden"
+                src="/assets/images/github.svg"
+                alt="GitHub logo"
+                width={17}
+                height={17}
+              />
+              <Image
+                draggable={false}
+                className="hidden dark:inline-block"
+                src="/assets/images/github_white.svg"
+                alt="GitHub logo"
+                width={17}
+                height={17}
+              />
+            </Button>
+            <Button
+              size="icon"
+              variant="outline"
+              className={`
+                size-10 backdrop-blur-none
+                not-first:rounded-s-none not-first:border-s-0
+                not-last:rounded-e-none not-last:border-e-0
+              `}
+              onClick={() => router.push("https://s.p0pa.de/yt")}
+            >
+              <Image
+                draggable={false}
+                className="dark:hidden"
+                src="/assets/images/youtube.svg"
+                alt="YouTube logo"
+                width={17}
+                height={17}
+              />
+              <Image
+                draggable={false}
+                className="hidden dark:inline-block"
+                src="/assets/images/youtube_white.svg"
+                alt="YouTube logo"
+                width={17}
+                height={17}
+              />
+            </Button>
+            <Button
+              size="icon"
+              variant="outline"
+              className={`
+                size-10 backdrop-blur-none
+                not-first:rounded-s-none not-first:border-s-0
+                not-last:rounded-e-none not-last:border-e-0
+              `}
+              onClick={() => router.push("https://s.p0pa.de/spotify")}
+            >
+              <Image
+                draggable={false}
+                className="dark:hidden"
+                src="/assets/images/spotify.svg"
+                alt="Spotify logo"
+                width={17}
+                height={17}
+              />
+              <Image
+                draggable={false}
+                className="hidden dark:inline-block"
+                src="/assets/images/spotify_white.svg"
+                alt="Spotify l7go"
+                width={17}
+                height={17}
+              />
+            </Button>
+          </nav>
+          <Button size="icon" variant="outline" className="size-10" onClick={toggleDarkMode}>
+            <Moon />
           </Button>
-          <Button
-            size="icon"
-            variant="outline"
-            className={`
-              size-10 backdrop-blur-none
-              not-first:rounded-s-none not-first:border-s-0
-              not-last:rounded-e-none not-last:border-e-0
-            `}
-            onClick={() => router.push("https://s.p0pa.de/yt")}
-          >
-            <Image
-              draggable={false}
-              className="dark:hidden"
-              src="/assets/images/youtube.svg"
-              alt="YouTube logo"
-              width={17}
-              height={17}
-            />
-            <Image
-              draggable={false}
-              className="hidden dark:inline-block"
-              src="/assets/images/youtube_white.svg"
-              alt="YouTube logo"
-              width={17}
-              height={17}
-            />
-          </Button>
-          <Button
-            size="icon"
-            variant="outline"
-            className={`
-              size-10 backdrop-blur-none
-              not-first:rounded-s-none not-first:border-s-0
-              not-last:rounded-e-none not-last:border-e-0
-            `}
-            onClick={() => router.push("https://s.p0pa.de/spotify")}
-          >
-            <Image
-              draggable={false}
-              className="dark:hidden"
-              src="/assets/images/spotify.svg"
-              alt="Spotify logo"
-              width={17}
-              height={17}
-            />
-            <Image
-              draggable={false}
-              className="hidden dark:inline-block"
-              src="/assets/images/spotify_white.svg"
-              alt="Spotify l7go"
-              width={17}
-              height={17}
-            />
-          </Button>
-        </nav>
+        </div>
       </div>
     </header>
   );
